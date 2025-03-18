f3c87a0f308b08f5064bf09d1675bc47fd6d5e98
lisa <njokuleesah@gmail.com>
2025-03-18 19:43:55 +0000

    trying framer motion out

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,14 +1,26 @@
 import face from '../images/face.jpg';
 import styled from 'styled-components';
 import { BasicLayout, Description, Hide, Image } from '../styles';
+import { motion } from 'framer-motion';
 
 const AboutSection = () =>{
+
+    const container = {
+        hidden :{ x : 100},
+        show :{x : 0, transition : {duration : 0.75, ease: "easeOut", staggerChildren: 0.2}}
+
+    };
+
+    const titleAnim ={
+        hidden : {opacity : 0},
+        show : {opacity :1, transition :{duration : 1} }
+    }
     return (
         <BasicLayout>
          <Description>
-            <div className="title">
+            <motion.div className="title" variants={container} initial ="hidden" animate="show">
                 <Hide>
-                    <h2>Maverick, Front-end developer,</h2>
+                    <motion.h2 variants={titleAnim}>Maverick, Front-end developer,</motion.h2>
                 </Hide>
 
                 <Hide>
@@ -19,7 +31,7 @@ const AboutSection = () =>{
                     <h2>The <span>Lis effect</span></h2>
                 </Hide>
 
-            </div>
+            </motion.div>
             <p> Contact me for scalable, high-performance 
             front-end solutions— responsive, optimized, and built to last.</p>
 
