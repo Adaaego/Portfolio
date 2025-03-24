01a0bc243da049f6727074895f7fc4c9397a6745
lisa <njokuleesah@gmail.com>
2025-03-24 19:53:04 +0000

    added animations to the heading, button and images in the about section

--- animations.jsx
+++ animations.jsx
@@ -9,13 +9,47 @@ export const PageAnimation ={
         transition: {
           duration: 0.5,
           when: "beforeChildren",
-          staggerChildren: 0.25,
+          staggerChildren: 0.5,
         },
       },
       exit: {
         opacity: 0,
         transition: {
           duration: 0.5,
+        },
+      },
+    };
+
+    export const titleAnim ={
+      hidden:{
+        opacity : 0,
+        y : 200
+      },
+      show :{
+        opacity : 1,
+        y: 0,
+        transition: { duration: 0.75, ease: "easeOut" },
+      }
+    };
+    
+    export const fade ={
+      hidden: {
+        opacity : 0,
+      },
+
+      show: {
+        opacity: 1,
+        transition: { ease: "easeOut", duration: 1},
+      }
+    };
+    export const photoAnim = {
+      hidden: { scale: 0, opacity: 0 },
+      show: {
+        scale: 0.8,
+        opacity: 1,
+        transition: {
+          ease: "easeOut",
+          duration: 1,
         },
       },
     };
--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,45 +1,44 @@
 import face from '../images/face.jpg';
 import styled from 'styled-components';
 import { BasicLayout, Description, Hide, Image } from '../styles';
+//Animations
 import { motion } from 'framer-motion';
+import { titleAnim, fade, photoAnim} from '../../animations';
+
+//Router 
+import { Link } from 'react-router-dom';
+
 
 const AboutSection = () =>{
 
-    const container = {
-        hidden :{ x : 100},
-        show :{x : 0, transition : {duration : 0.75, ease: "easeOut", staggerChildren: 0.2}}
-
-    };
-
-    const titleAnim ={
-        hidden : {opacity : 0},
-        show : {opacity :1, transition :{duration : 1} }
-    }
+    
     return (
         <BasicLayout>
          <Description>
-            <motion.div className="title" variants={container} initial ="hidden" animate="show">
+            <motion.div>
                 <Hide>
                     <motion.h2 variants={titleAnim}>Maverick, Front-end developer,</motion.h2>
                 </Hide>
 
                 <Hide>
-                    <h2>Always evolving.</h2>
+                    <motion.h2 variants={titleAnim}>Always evolving.</motion.h2>
                 </Hide>
 
                 <Hide>
-                    <h2>The <span>Lis effect</span></h2>
+                    <motion.h2 variants={titleAnim}>The <span>Lis effect</span></motion.h2>
                 </Hide>
 
             </motion.div>
-            <p> Contact me for scalable, high-performance 
-            front-end solutions— responsive, optimized, and built to last.</p>
-
-            <button>Contact Me</button>
+            <motion.p variants={fade}> Contact me for scalable, high-performance 
+            front-end solutions— responsive, optimized, and built to last.</motion.p>
+            <Link to="/Contact">
+             <motion.button variants={fade}>Contact Me</motion.button>
+            </Link>
+            
          </Description>
 
          <Image>
-            <img src={face} alt="guy holding camera" />
+            <motion.img variants={photoAnim} src={face} alt="guy holding camera" />
          </Image>
         </BasicLayout>
     )
