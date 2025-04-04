8f185866d526c23e842b2f7b3d484419bc4eda2c
lisa <njokuleesah@gmail.com>
2025-04-04 19:35:50 +0000

    added content and animations to the contact page

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,13 +1,77 @@
 //animations 
 import { motion } from "framer-motion";
-import { PageAnimation } from "../../animations";
+import { PageAnimation, titleAnim  } from "../../animations";
+import styled from "styled-components";
+
 
 const ContactMe = () =>{
     return(
-        <motion.div  variants={PageAnimation} initial ="hidden" animate="show" exit = "exit">
-            <h1>You can reach me on 0535702336</h1>
-        </motion.div>
-    )
-}
-
-export default ContactMe;
+        <ContactStyle
+        exit="exit"
+        variants={PageAnimation}
+        initial="hidden"
+        animate="show"
+      >
+        <Title>
+          <Hide>
+            <motion.h2 variants={titleAnim}>Get in touch.</motion.h2>
+          </Hide>
+        </Title>
+        <div>
+          <Hide>
+            <Social variants={titleAnim}>
+              <Circle />
+              <h2>Send Us A Message</h2>
+            </Social>
+          </Hide>
+          <Hide>
+            <Social variants={titleAnim}>
+              <Circle />
+              <h2>Send an email.</h2>
+            </Social>
+          </Hide>
+          <Hide>
+            <Social variants={titleAnim}>
+              <Circle />
+              <h2>Social Media</h2>
+            </Social>
+          </Hide>
+        </div>
+      </ContactStyle>
+    );
+  };
+  
+  const ContactStyle = styled(motion.div)`
+    padding: 5rem 10rem;
+    color: #353535;
+    min-height: 90vh;
+    @media (max-width: 1500px) {
+      padding: 2rem;
+      font-size: 1rem;
+    }
+  `;
+  const Title = styled.div`
+    margin-bottom: 4rem;
+    color: black;
+    @media (max-width: 1500px) {
+      margin-top: 5rem;
+    }
+  `;
+  const Hide = styled.div`
+    overflow: hidden;
+  `;
+  const Circle = styled.div`
+    border-radius: 50%;
+    width: 3rem;
+    height: 3rem;
+    background: #ffffff;
+  `;
+  const Social = styled(motion.div)`
+    display: flex;
+    align-items: center;
+    h2 {
+      margin: 2rem;
+    }
+  `;
+  
+  export default ContactMe;
